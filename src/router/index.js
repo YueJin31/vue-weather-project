0e1843107ad7c58b26440cf92566ae7bfbb246ad
YueJin <yuejin07@gmail.com>
2022-12-13 16:58:27 +0200

    path

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/vue-weather-project/favorites",
+    path: "/favorites",
     name: "favorites",
     component: FavoritesView,
   },
